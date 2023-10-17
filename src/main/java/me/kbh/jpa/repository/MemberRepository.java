ac9e94f9c0482d389489e7dc6b90fb776f7bb97c
jvm-kbh <elasticsearch@kakao.com>
2023-10-17 15:31:47 +0900

    @Query 매칭해보기

--- src/main/java/me/kbh/jpa/repository/MemberRepository.java
+++ src/main/java/me/kbh/jpa/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package me.kbh.jpa.repository;
 
 import me.kbh.jpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     //@Query(name = "Member.findByUsername") 생략가능하고 메서드 명으로 매칭 가능하다.
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
